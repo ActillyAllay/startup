b04406f8a6bb2e58b0c9cd5a52392a4c767ecb03
ActillyAllay <cassiemrust@gmail.com>
2026-02-14 23:23:13 -0700

    docs: Created individual page .jsx files

---
+++ src/login/login.jsx
@@ -0,0 +1,10 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import React from 'react';
+
+export function Login() {
+  return (
+    <main className="container-fluid bg-secondary text-center">
+      <div>login displayed here</div>
+    </main>
+  );
+}
---
+++ src/playlists/playlists.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export function Playlists() {
+  return (
+    <main className="container-fluid bg-secondary text-center">
+      <div>playlists displayed here</div>
+    </main>
+  );
+}
