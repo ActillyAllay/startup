97cc67ef997f4bc4e0b133f64a784b343395d726
ActillyAllay <cassiemrust@gmail.com>
2026-03-01 22:01:03 -0700

    feat: Song queue reacts to local storage in music.jsx; based on simon-react scores code

--- src/music/music.jsx
+++ src/music/music.jsx
@@ -1,6 +1,42 @@
 import React from 'react';
 
+function Queue() {
+
+  localStorage.setItem("queued", JSON.stringify([["one", "author1", "3:56"], ["two", "author2", "1:40"]]))
+  
+  const [queued, setQueued] = React.useState([]);
+  const queueList = [];
+  
+  React.useEffect(() => {
+    const storedQueue = localStorage.getItem('queued');
+    if (storedQueue) {
+      setQueued(JSON.parse(storedQueue));
+    }
+  }, []);
+
+  if (queued.length) {
+    for (const [i, song] of queued.entries()) {
+      queueList.push(
+        <tr key={i}>
+          <td>{song[0]}</td>
+          <td>{song[1]}</td>
+          <td><button>&uarr;</button><button>&darr;</button></td>
+          <td><button>X</button></td>
+        </tr>
+      )
+    }
+    return queueList;
+  } else {
+    return (
+      <tr>
+        <td>No songs in the queue</td>
+      </tr>
+    )
+  }
+}
+
 export function Music() {
+
   return (
     <main>
       <div id="gridWrapper">
@@ -20,42 +56,7 @@ export function Music() {
           <h3>Queue</h3>
           <table>
           <tbody>           
-            <tr>
-              <td>Dreams of Our Generation</td>
-              <td>(Placeholder for Song Info)</td>
-              <td><button>&uarr;</button><button>&darr;</button></td>
-              <td><button>X</button></td>
-            </tr>
-            <tr>
-              <td>Different Song</td>
-              <td>(Placeholder for Song Info)</td>
-              <td><button>&uarr;</button><button>&darr;</button></td>
-              <td><button>X</button></td>
-            </tr>
-            <tr>
-              <td>Different Song</td>
-              <td>(Placeholder for Song Info)</td>
-              <td><button>&uarr;</button><button>&darr;</button></td>
-              <td><button>X</button></td>
-            </tr>
-            <tr>
-              <td>Different Song</td>
-              <td>(Placeholder for Song Info)</td>
-              <td><button>&uarr;</button><button>&darr;</button></td>
-              <td><button>X</button></td>
-            </tr>
-            <tr>
-              <td>Different Song</td>
-              <td>(Placeholder for Song Info)</td>
-              <td><button>&uarr;</button><button>&darr;</button></td>
-              <td><button>X</button></td>
-            </tr>
-            <tr>
-              <td>Different Song</td>
-              <td>(Placeholder for Song Info)</td>
-              <td><button>&uarr;</button><button>&darr;</button></td>
-              <td><button>X</button></td>
-            </tr>
+            {<Queue />}
           </tbody>
           </table>
           <br />
